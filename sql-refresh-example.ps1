b31539164a8840d0af13892b0ed91c02c166821e
Mike Fal <mike@mikefal.net>
2017-08-06 15:47:15 -0600

    Add DBSizing queries, demo of refreshing a database env with Rubrik, and script to execute Kroll restore.

---
+++ sql-refresh-example.ps1
@@ -0,0 +1,35 @@
+﻿#Load Rubrik Module
+import-module Rubrik
+
+#set credential to log in to Rubrik
+$cred = Get-Credential 
+
+#Connect to Rubrik server
+Connect-Rubrik -Server <RUBRIK IP/DNS NAME> -Credential $cred 
+
+#Export all dbs to another instance, no file relocation
+$SourceServer = 'SERVERA'
+$TargetServer = 'SERVERB'
+
+$db = Get-RubrikDatabase -Hostname $SourceServer -Instance 'MSSQLSERVER' | Where-Object {@('master','model','msdb') -notcontains $_.Name -and $_.isRelic -ne 'True'}
+$TargetInstanceID = (Get-RubrikDatabase -Hostname $TargetServer -Instance 'MSSQLSERVER' -Database 'master').instanceId
+
+$db | Export-RubrikDatabase -RecoveryDateTime (Get-Date '2017-06-26T12:00:00') -TargetInstanceId $TargetInstanceID -WhatIf
+#If the above -WhatIf output is good, replace -WhatIf with -Confirm:$false
+
+
+#To create live mounts, use this instead of the Export-RubrikDatabase
+#only support in Rubrik 4.0
+$db | New-RubrikDatabaseMount -RecoveryDateTime (Get-Date '2017-06-26T12:00:00') -TargetInstanceId $TargetInstanceID -MountedDatabaseName "$_`LM" -WhatIf
+
+#If you need to drop databases before Export/Restore
+$db.name | ForEach-Object {Invoke-Sqlcmd -ServerInstance $TargetServer -Database tempdb -Query 'DROP DATABASE $_;'}
+
+#To Execute post export/mount scripts
+#capture outupt of Export-RubrikDatabase call, these are there the async requests
+#you will want to check to see if all the requests are 'SUCCEEDED'
+
+$reqcount = ($requests | Where-Object {$_.status -ne 'SUCCEEDED'} | Measure-Object).Count
+if($reqcount -eq 0){
+    $db.name | ForEach-Object {Invoke-Sqlcmd -ServerInstance $TargetServer -Database tempdb -InputFile C:\SQLFiles\postexport.sql}
+}
