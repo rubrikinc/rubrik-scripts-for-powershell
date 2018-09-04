2e6fe247df02d1a5e077a8add5910b73c7dd7646
Mike Fal <mike@mikefal.net>
2018-09-04 11:03:54 -0600

    Script to run a series of database exports from one instance to another.

---
+++ invoke-masssqlrestore.ps1
@@ -0,0 +1,29 @@
+﻿#Requires -Modules Rubrik
+param([Parameter(Mandatory=$true)]
+      [string[]]$databases
+     ,[Parameter(Mandatory=$true)]
+      [string]$SourceInstance
+     ,[Parameter(Mandatory=$true)]
+      [String]$TargetInstance
+     ,[string]$TargetDataFilePath
+     ,[string]$TargetLogFilePath
+     )
+
+$Target = Get-RubrikSQLInstance -ServerInstance $TargetInstance
+$dbs = Get-RubrikDatabase -ServerInstance $SourceInstance |
+       Where-Object {$databases -contains $_.name -and $_.isRelic -ne 'TRUE' -and $_.isLiveMount -ne 'TRUE'} |
+       Get-RubrikDatabase
+$reqs = @()
+
+foreach($db in $dbs){
+    $reqs += Export-RubrikDatabase -Id $db.id `
+                          -RecoveryDateTime (Get-Date $db.latestRecoveryPoint) `
+                          -FinishRecovery `
+                          -TargetInstanceId $Target.id `
+                          -TargetDataFilePath $TargetDataFilePath `
+                          -TargetLogFilePath $TargetLogFilePath `
+                          -TargetDatabaseName $db.name `
+                          -Confirm:$false                         
+}
+
+return $reqs
