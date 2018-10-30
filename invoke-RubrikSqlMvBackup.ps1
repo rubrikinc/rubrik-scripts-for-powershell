5af3167c105fb0b7de30f6d15321efbb74235d85
Mike Fal <mike@mikefal.net>
2018-10-30 11:29:06 -0600

    Simple cmdlets for performing SQL Server backups to managed volumes in CDM 5.0

---
+++ invoke-RubrikSqlMvBackup.ps1
@@ -0,0 +1,28 @@
+﻿param([String]$MVName
+      ,[String]$ServerInstance
+      ,[String]$Database
+      ,[switch]$DeleteOld
+      ,[switch]$script
+      ,[int]$MaxTransferSize
+      ,[int]$BufferCount)
+
+$i=0
+$mv = Get-RubrikManagedVolume -Name $MVName
+$files = $mv.mainExport.channels | 
+    ForEach-Object {$i++;"DISK='\\$($_.ipaddress)\$($_.mountpoint)\$Database`_$i`_$(Get-Date -Format 'yyyyMMddHHmmss').bak'"}
+
+$sql = "BACKUP DATABASE [$Database] TO " + ($files -join ',') + " WITH INIT,COMPRESSION,STATS=10"
+if($MaxTransferSize){$sql+=",MAXTRANSFERSIZE=$MaxTransferSize"}
+if($BufferCount){$sql+=",BUFFERCOUNT=$BufferCount"}
+if($script){
+    Write-Output $sql
+} else {
+$mv | Start-RubrikManagedVolumeSnapshot
+if($DeleteOld){
+    foreach($channel in $mv.mainExport.channels){
+        Get-ChildItem -Path "\\$($channel.ipaddress)\$($channel.mountpoint)\$Database_*.bak" | Remove-Item -Force
+    }
+}
+    sqlcmd -S $ServerInstance -Q $sql
+    $mv | Stop-RubrikManagedVolumeSnapshot
+}
