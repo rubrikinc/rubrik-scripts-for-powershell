85739d0f5ec2a27dbffa9406da71d61b059f2d02
Peter J. Milanese <peter.milanese@rubrik.com>
2019-04-05 13:45:55 -0400

    Update report, marked up.

--- Reporting/get_archive_report.ps1
+++ Reporting/get_archive_report.ps1
@@ -1,21 +1,31 @@
+#
+# Name:     get_archive_report.ps1
+# Author:   pmilano1 (Peter J. Milanese)
+# Use case: Basic report on failed backups for the last 24 hours
+# Ex: .\get_archive_report.ps1 -rubrik [FDQN|IP Address for Rubrik Cluster] [-SLA [sla1,sla2,sla3]]
+
 param (
     [string]$rubrik = $(Read-Host -Prompt 'Input your Rubrik IP or Hostname'),
     [string]$SLA = $(Read-Host -Prompt "Enter SLAs ['SLA1','SLA2,'etc'] or just hit enter to report on all")
 )
 
-#$items_to_report = ('VmwareVirtualMachine','Mssql','LinuxFileset','WindowsFileset','NasFileset')
-$items_to_report = @{
- "VmwareVirtualMachine" = @{"url" = "/v1/vmware/vm/{0}/snapshot"; "array"  = "data"}
- "Mssql" = @{"url" = "/v1/mssql/db/{0}/snapshot"; "array" = "data"}
- "LinuxFileset" = @{"url" = "/v1/fileset/{0}"; "array" = "snapshot"}
- "WindowsFileset" = @{"url" = "/v1/fileset/{0}"; "array" = "snapshot"}
- "NasFileset" = @{"url" = "/v1/fileset/{0}"; "array" = "snapshot"}
- "ManagedVolume" = @{"url" = "/internal/managed_volume/{0}/snapshot"; "array" = "data"}
+# Static hash of object information
+$objtype = @{
+ "VmwareVirtualMachine" = @{"url" = "v1/vmware/vm/{0}/snapshot"; "array"  = "data"}
+ "Mssql" = @{"url" = "v1/mssql/db/{0}/snapshot"; "array" = "data"}
+ "LinuxFileset" = @{"url" = "v1/fileset/{0}"; "array" = "snapshot"}
+ "WindowsFileset" = @{"url" = "v1/fileset/{0}"; "array" = "snapshot"}
+ "NasFileset" = @{"url" = "v1/fileset/{0}"; "array" = "snapshot"}
+ "ManagedVolume" = @{"url" = "internal/managed_volume/{0}/snapshot"; "array" = "data"}
 }
-$selected = ( "Location", "ObjectName", "ObjectId", "SlaDomain")
 
+# Fields from Report to keep in the output
+$selected = ( "Location", "ObjectName", "SlaDomain")
 
+# Force TLS1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Bypass cert verification
 if (-not("dummy" -as [type])) {
     add-type -TypeDefinition @"
 using System;
@@ -28,20 +38,19 @@ public static class Dummy {
         X509Certificate certificate,
         X509Chain chain,
         SslPolicyErrors sslPolicyErrors) { return true; }
-
     public static RemoteCertificateValidationCallback GetDelegate() {
         return new RemoteCertificateValidationCallback(Dummy.ReturnTrue);
     }
 }
 "@
 }
-
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = [dummy]::GetDelegate()
 
+# Static Variables
 $limit = 100
 $object_report_name = "Object Protection Summary"
 
-# Check for Credentials
+# Check for Credentials and prompt if need be
 New-Item -ItemType Directory -Force -Path "$($PSScriptRoot)\.creds" | out-null
 $Credential = ''
 $CredentialFile = "$($PSScriptRoot)\.creds\$($rubrik).cred"
@@ -55,15 +64,31 @@ else {
   $Credential | Export-CliXml -Path $CredentialFile
 }
 
+# Setup Auth Header
 $auth = [System.Text.Encoding]::UTF8.GetBytes(("{0}:{1}" -f $Credential.UserName.ToString(),([Runtime.InteropServices.Marshal]::PtrToStringBSTR([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password)))))
 $auth = [System.Convert]::ToBase64String($auth)
 $headers = @{
   Authorization = "Basic {0}" -f $auth
   Accept = 'application/json'
 }
+
+# Function to get the latest archive date, return 'None' if none. 
+function get_latest_archive_info  ($rubrik,$id, $a){
+  $uri = [uri]::EscapeUriString("https://$($rubrik)/api/$($objtype[$a].url -f $id)")
+  $r = Invoke-RestMethod -Headers $headers -Method GET -Uri $uri 
+  foreach ($snap in $r.$($objtype[$a].array)){
+    if ($snap.cloudState -eq 2){
+      return $snap.date
+      break
+    }
+  }
+  return "None"
+}
+
+# Function to get all objects, filter by SLA array if provided
 function get-rubrik-objects ($rubrik, $object_report_id, $SLA) {
   $out = @{}
-  $out.report_data = @()
+  $out.rd = @()
   $payload = @{
     "limit" = $limit
   }
@@ -73,24 +98,25 @@ function get-rubrik-objects ($rubrik, $object_report_id, $SLA) {
     if ($null -ne $cursor){
       $payload['cursor'] = $cursor
     }
-    $report_results = Invoke-RestMethod -Headers $headers -Method POST -Uri $($uri) -Body $(convertto-json $payload) 
-    $cursor = $report_results.cursor
-    $hasMore = $report_results.hasMore
-    $out.report_columns = $report_results.columns
+    $rr = Invoke-RestMethod -Headers $headers -Method POST -Uri $($uri) -Body $(convertto-json $payload) 
+    $cursor = $rr.cursor
+    $hasMore = $rr.hasMore
+    $out.rc = $rr.columns
     if ($SLA){
-      foreach ($l in $report_results.dataGrid){
-        if ($SLA -contains $l[$report_results.columns.indexOf('SlaDomain')]){
-          $out.report_data += ,$l
+      foreach ($l in $rr.dataGrid){
+        if ($SLA -contains $l[$rr.columns.indexOf('SlaDomain')]){
+          $out.rd += ,$l
         }
       }
     }
     else{
-      $out.report_data += $report_results.dataGrid
+      $out.rd += $rr.dataGrid
     }
   }
   return ($out)
 }
 
+# Get the report ID to looking Objects from
 try{
   $uri = [uri]::EscapeUriString("https://$($rubrik)/api/internal/report?name=$($object_report_name)")
   $object_report_list = Invoke-RestMethod -Headers $headers -Method GET -Uri $uri 
@@ -105,48 +131,26 @@ catch{
   write-host "Got : $($_.Exception)"
 }
 
+# Establish the hash of objects from the report
 if ($object_report_id){
   $rr = get-rubrik-objects $rubrik $object_report_id $SLA
 }
 
-$columns = $selected
-$columns += "LastSnapshot"
-$columns += "LastSnapshotStatus"
-$columns += "LastArchive"
-$columns += "LastArchiveStatus"
-$columns_out = '"{0}"' -f ($columns -join '","')
-Write-Host $columns_out
+# Setup and output the header columns
+$c = $selected
+$c += "ArchiveDate"
+$co = '"{0}"' -f ($c -join '","')
+Write-Host $co
 
-foreach ($report_line in $rr.report_data){
-  $last_status = ''
-  if ($items_to_report.Keys -notcontains $report_line[$($rr.report_columns.indexOf("ObjectType"))]){ continue }
-  $report_out = @()
+# Loop the report results, merge in the last archive data, output as csv 
+foreach ($l in $rr.rd){
+  if ($objtype.Keys -notcontains $l[$rr.rc.indexOf('ObjectType')]){continue}
+  $ro = @()
   foreach($f in $selected){
-    $report_out += $report_line[$($rr.report_columns.indexOf($f))]
+    $ro += $l[$($rr.rc.indexOf($f))]
   }
-  $object_id = [uri]::EscapeDataString($report_line[$rr.report_columns.indexOf('ObjectId')])
-  $uri = "https://$($rubrik)/api/internal/event?limit=1&event_type=Backup&object_ids=$($object_id)"
-  $event_results = (Invoke-RestMethod -Headers $headers -Method GET -Uri $($uri)) 
-  if (($event_results.data).count -eq 0){
-    $report_out += "None Found"
-    $report_out += "None Found"
-  }
-  else{
-    $report_out += "$($event_results.data.time)"
-    $report_out += "$($event_results.data.eventStatus)"
-  }
-  $uri = "https://$($rubrik)/api/internal/event?limit=1&event_type=Archive&object_ids=$($object_id)"
-  $event_results = (Invoke-RestMethod -Headers $headers -Method GET -Uri $($uri)) 
-  if (($event_results.data).count -eq 0){
-    $report_out += "None Found"
-    $report_out += "None Found"
-  }
-  else{
-    $report_out += "$($event_results.data.time)"
-    $report_out += "$($event_results.data.eventStatus)"
-  }
-  $report_out = '"{0}"' -f ($report_out -join '","')
-  Write-Host $report_out
+  $ro += (get_latest_archive_info $rubrik $l[$rr.rc.indexOf('ObjectId')] $l[$rr.rc.indexOf('ObjectType')])
+  $ro = '"{0}"' -f ($ro -join '","')
+  Write-Host $ro
 }
-
 exit
