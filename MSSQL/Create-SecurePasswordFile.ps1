2b841be05429fe8fd4819fda63a059a063cb61fe
Jaap Brasser <noreply@jaapbrasser.com>
2019-05-30 20:10:15 +0200

    Removed .vscode and added synopsis

--- MSSQL/Create-SecurePasswordFile.ps1
+++ MSSQL/Create-SecurePasswordFile.ps1
@@ -1,2 +1,5 @@
-﻿$Credential = Get-Credential
+﻿<#
+.SYNOPSIS Prompts for credentials securely and stores the credentials in an encrypted file, Rubrik.cred
+#>
+$Credential = Get-Credential
 $Credential | Export-CliXml -Path .\rubrik.Cred
