18c6735acc978e85d0b3c82ce52e5bd59b2df2a6
fernandesdba <fernandes_dba@hotmail.com>
2019-07-01 19:24:29 +0200

    Created new MassLivemount and unMount script adding new parameters and considering logBackup as well

---
+++ MSSQL/invoke-MassLiveMount.ps1
@@ -0,0 +1,95 @@
+<#
+    .SYNOPSIS
+        Script to start Live Mount for a given databases
+    .DESCRIPTION
+        Script will Live Mount given databases from a SQL Server instance to a given target adding the suffix "_LM" at the LiveMounted database
+        The script will unmount previous database for databases in the parameter $databases
+    .PARAMETER databases
+        Mandatory parameter, can be a array of database names    
+    .PARAMETER SourceServerInstance
+        SQL Server Instance name where the given database is hosted, this is the Source SQL Server instance. If default instance, then just use the server name, if a named instance, use
+        Server\Instance
+    .PARAMETER TargetServerInstance
+        SQL Server Instance name where the databases will be mounted, this is the Target SQL Server instance. If default instance, then just use the server name, if a named instance, use
+        Server\Instance 
+    .PARAMETER RecoveryDateTime
+        optional parameter, should be used if you want to Mount an specif point in time,
+        The default Point In Time it will be the "Latest Recovery Point" if this parameter is not informed
+    .EXAMPLE
+        Example of how to run the script   
+        
+        Live Mounting databases using an specific point in time
+        .\invoke-MassLiveMount.ps1 -databases ("tpcc","AdventureWorks2014","pubs","dbTest","dbTest2") `
+                -SourceServerInstance "MF-SQL17-test" `
+                -TargetServerInstance "MF-SQL17-01"`
+                -RecoveryDateTime "07/01/2019 15:01:38" `
+                -Verbose 
+
+        Live Mounting databases using the latest recovey point
+        .\invoke-MassLiveMount.ps1 -databases ("tpcc","AdventureWorks2014","pubs","dbTest","dbTest2") `
+                -SourceServerInstance "MF-SQL17-test" `
+                -TargetServerInstance "MF-SQL17-01"`
+                -Verbose  
+    .LINK
+        None
+    .NOTES
+    Execution Process:
+    Before running this script, you need to connect to the Rubrik cluster, and also ensure SQL Permission for account that will run this script.
+        Ex.
+        $RubrikServer = "172.21.8.51"
+        #$RubrikCredential = (Get-Credential -Message 'Please enter your Rubrik credentials')
+        Connect-Rubrik -Server $RubrikServer -Credential $RubrikCredential
+
+        Created:    2019-07-01
+        Author:     Marcelo Fernandes
+        Notes:      Added parameter for Point In Time restore and changed the script to consider Log Backup as well (previously it was considering only snapshots)
+#>
+
+param(   [Parameter(Mandatory=$true)]
+         [string[]]$databases
+        ,[Parameter(Mandatory=$true)]
+         [String]$SourceServerInstance
+        ,[String]$TargetServerInstance
+        ,[ValidateScript({get-date $_ })] 
+         [datetime]$RecoveryDateTime
+        )
+
+Function ConvertFrom-ServerInstance($ServerInstance) {
+    if($ServerInstance -contains '\'){
+        $si = $ServerInstance -split '\'
+        $return = New-Object psobject -Property @{'hostname'= $si[0];'instancename'=$si[1]}
+    } else {
+        $return = New-Object psobject -Property @{'hostname'= $ServerInstance;'instancename'='MSSQLSERVER'}
+    }
+    return $return
+}
+
+Function Wait-RubrikRequests($reqs) {
+    do{
+        Start-Sleep -Seconds 15
+        $reqs = $reqs | Get-RubrikRequest -Type mssql -ErrorAction SilentlyContinue
+    }until(($reqs | Where-Object {@('QUEUED','RUNNING','FINISHING') -contains $_.status} | Measure-Object).Count -eq 0)
+}
+
+$source = ConvertFrom-ServerInstance $SourceServerInstance
+$TargetInstance = (Get-RubrikSQLInstance -ServerInstance $TargetServerInstance)
+
+Write-Verbose "Begining unmount/cleanup process for: $($databases -join ",")"
+$unmount_reqs = Get-RubrikDatabaseMount -TargetInstanceId $TargetInstance.id | 
+    Where-Object {$databases -contains $_.sourceDatabaseName -and $_.isrelic -ne 'TRUE'} |
+    Remove-RubrikDatabaseMount -Confirm:$false 
+
+if($unmount_reqs) {Wait-RubrikRequests $unmount_reqs}
+
+#Starting Live Mount DBs
+"Begining mount process for: $($databases -join ",")" | Out-Host
+
+$sourceDBs = Get-RubrikDatabase -Hostname $source.hostname -Instance $source.instancename |Where-Object {$databases -contains $_.Name -and $_.isrelic -ne 'TRUE' -and $_.isLiveMount -ne 'TRUE'} 
+$mount_reqs = $sourceDBs | Get-RubrikDatabase |
+    ForEach-Object{
+    $date = if($RecoveryDateTime){ $RecoveryDateTime.ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ') }else{ $_.latestRecoveryPoint};
+    write-Verbose "Livemounting $($_.name) -Recovery Point- $(get-date $date) UTC";
+    try{
+        New-RubrikDatabaseMount -TargetInstanceId $TargetInstance.id -MountedDatabaseName "$($_.name)_LM" -RecoveryDateTime $date -id $_.id -confirm:$false
+       }catch{$_}}
+if($mount_reqs) {Wait-RubrikRequests $mount_reqs}
---
+++ MSSQL/invoke-MassUnMount.ps1
@@ -0,0 +1,78 @@
+<#
+.SYNOPSIS
+mass-unMount is used to unMount multiple databases.
+
+.DESCRIPTION
+Based on parameters supplied, mass-unMount will start unmounting LM databases for supplied databases or whole instance.
+
+.PARAMETER databases
+It is an optional parameter, if is supplied, the script will start unMount LM DBs only for supplied DBs on this parameter. 
+If this parameter was not supplied, the script will consider all LM databases supplied on ServerInstance parameter
+
+.PARAMETER ServerInstance
+SQL Server Instance name that has the LM DBs, e.g. SQLserv01\Prod
+
+.EXAMPLE
+Start unMount LM DBs for supplied databases.
+    $RubrikServer = "172.21.8.51"
+    $RubrikCredential = (Get-Credential -Message 'Please enter your Rubrik credentials')
+    Connect-Rubrik -Server $RubrikServer -Credential $RubrikCredential
+    .\mass-unMount.ps1 -databases "AdventureWorks2014","DBTest01","DBTest02" -ServerInstance "sql1.domain.com"
+        
+.EXAMPLE
+Start unMount all LM DBs on the instance.
+    $RubrikServer = "172.21.8.51"
+    $RubrikCredential = (Get-Credential -Message 'Please enter your Rubrik credentials')
+    Connect-Rubrik -Server $RubrikServer -Credential $RubrikCredential
+    .\mass-unMount.ps1 -ServerInstance "sql1.domain.com"
+
+.NOTES
+Name:               Mass unMount Live Mounted databases
+Created:            09/13/2018
+Author:             Marcelo Fernandes
+Execution Process:
+    Before running this script, you need to connect to the Rubrik cluster, and also ensure SQL Permission for account that will run this script.
+        Ex.
+        $RubrikServer = "172.21.8.51"
+        #$RubrikCredential = (Get-Credential -Message 'Please enter your Rubrik credentials')
+        Connect-Rubrik -Server $RubrikServer -Credential $RubrikCredential
+#>
+
+param( [String[]] $databases
+        ,[Parameter(Mandatory=$true)]
+        [String] $ServerInstance              
+        )
+
+
+function ConvertFrom-ServerInstance($ServerInstance) {
+    if($ServerInstance -contains '\'){
+        $si = $ServerInstance -split '\'
+        $return = New-Object psobject -Property @{'hostname'= $si[0];'instancename'=$si[1]}
+    } else {
+        $return = New-Object psobject -Property @{'hostname'= $ServerInstance;'instancename'='MSSQLSERVER'}
+    }
+    return $return
+}
+
+function Wait-RubrikRequests($reqs) {
+    do{
+        Start-Sleep -Seconds 15
+        $reqs = $reqs | Get-RubrikRequest -Type mssql -ErrorAction SilentlyContinue
+    }until(($reqs | Where-Object {@('QUEUED','RUNNING','FINISHING') -contains $_.status} | Measure-Object).Count -eq 0)
+}
+
+$source = ConvertFrom-ServerInstance $ServerInstance
+$TargetInstance = (Get-RubrikSQLInstance -ServerInstance $source)
+#Getting user databases by instance if the $databases parameter was not informed
+if(!$databases){
+    $databases=@{}
+    $dbExclusion ="master","Tempdb","model","msdb","distribution"
+    $databases = $(Get-RubrikDatabase -Hostname $source.hostname -Instance $source.instance | Where-Object {$_.isrelic -ne 'TRUE' -and $_.isLiveMount -ne 'TRUE' -and $_.name  -notmatch [String]::Join('|', $dbExclusion)}| Select-Object name).name
+}
+
+"Begining unmount/cleanup process for: $($databases -join ",")" | Out-Host
+$unmount_reqs = Get-RubrikDatabaseMount -TargetInstanceId $TargetInstance.id |
+    Where-Object {$databases -contains $_.sourceDatabaseName} |
+    Remove-RubrikDatabaseMount -Confirm:$false
+
+if($unmount_reqs) {Wait-RubrikRequests $unmount_reqs}
