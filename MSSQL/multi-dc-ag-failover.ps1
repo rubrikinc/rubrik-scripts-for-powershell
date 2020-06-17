4efbb35ff9dec9c72267c432fb8c730e45c4d7d0
Mike F <sake.god@gmail.com>
2020-06-17 16:43:14 -0600

    Changed connection process to leverage splatting

--- MSSQL/multi-dc-ag-failover.ps1
+++ MSSQL/multi-dc-ag-failover.ps1
@@ -4,7 +4,8 @@
 .DESCRIPTION
     This script is designed to enable or disable protection across an availability group
     to support cross data center failover. This script assumes that all the databases in the AG
-    are inheriting from the AG and no databases are directly assigned.
+    are inheriting from the AG and no databases are directly assigned. It also assumes that the
+    SLA domain name is the same for both sides of the AG.
 .EXAMPLE
     .\multi-dc-ag-failover -AGname YourAG -PrimaryRubrikCluster cluster1.yourdomain.com -PrimaryRubrikToken '0000-0000-0000-0000-0000' -SecondaryRubrikCluster cluster2.yourdomain.com -SecondaryRubrikToekn '0000-0000-0000-0000-0000'
         -SLAName 'YourSLA' -LogBackupFrequencyMin 15 -LogBackupRetentionDays 14
@@ -20,9 +21,7 @@ param(#Availability Group Name
       [Parameter(ParameterSetName='Core')]
       [string]$PrimaryRubrikCluster,
       [Parameter(ParameterSetName='Core')]
-      [string]$PrimaryRubrikUser,
-      [Parameter(ParameterSetName='Core')]
-      [securestring]$PrimaryRubrikPassword,
+      [pscredential]$PrimaryRubriCredential,
       [Parameter(ParameterSetName='Core')]
       [string]$PrimaryRubrikToken,
 
@@ -30,9 +29,7 @@ param(#Availability Group Name
       [Parameter(ParameterSetName='Core')]
       [string]$SecondaryRubrikCluster,
       [Parameter(ParameterSetName='Core')]
-      [string]$SecondaryRubrikUser,
-      [Parameter(ParameterSetName='Core')]
-      [securestring]$SecondaryRubrikPassword,
+      [pscredential]$SecondaryRubriCredential,
       [Parameter(ParameterSetName='Core')]
       [string]$SecondaryRubrikToken,
 
@@ -52,23 +49,40 @@ param(#Availability Group Name
       [Switch]$NewSnapshot
       )
 
-#connect to the cluster that will be the secondary
-if($SecondaryRubrikToken.Length -gt 0){
-    Connect-Rubrik -Server $SecondaryRubrikCluster -Token $SecondaryRubrikToken | Out-Null
-} else {
-    Connect-Rubrik -Server $SecondaryRubrikCluster -Username $SecondaryRubrikUser -Password @SecondaryRubrikPassword| Out-Null
+function New-RubrikConnection{
+    param($server,$cred,$token)
+    switch($true){
+        {$cred} {
+            $return = @{
+                Server = $server
+                Credential = $cred
+            }
+        }
+        {$token} {
+            $return = @{
+                Server = $server
+                Token = $token
+            }
+        }
+        default {
+            $return = @{
+                Server = $RubrikServer
+            }
+        }
+    }
+    return $return
 }
+#connect to the cluster that will be the secondary
+$connection = New-RubrikConnection -server $SecondaryRubrikCluster -cred $SecondaryRubriCredential -token $SecondaryRubrikToken
+Connect-Rubrik @connection
 #gather AG
 $secondaryag = Get-RubrikAvailabilityGroup -GroupName $agname
 Set-RubrikAvailabilityGroup -id $secondaryag.id -SLA UNPROTECTED -LogBackupFrequencyInSeconds 0 -LogRetentionHours 0 -Confirm:$false
 
-
 #connect to the cluster that will be the primary
-if($SecondaryRubrikToken.Length -gt 0){
-    Connect-Rubrik -Server $PrimaryRubrikCluster -Token $PrimaryRubrikToken | Out-Null
-} else {
-    Connect-Rubrik -Server $PrimaryRubrikCluster -Username $PrimaryRubrikUser -Password @PrimaryRubrikPassword| Out-Null
-}
+$connection = New-RubrikConnection -server $PrimaryRubrikCluster -cred $PrimaryRubriCredential -token $PrimaryRubrikToken
+Connect-Rubrik @connection
+
 #Enable protection on primary Rubrik cluster
 $primaryag = Get-RubrikAvailabilityGroup -GroupName $agname
 Set-RubrikAvailabilityGroup -id $primaryag.id -SLA $SLAName -LogBackupFrequencyInSeconds ($LogBackupFrequencyMin * 60) -LogRetentionHours ($LogBackupRetentionDays * 24) -Confirm:$false
