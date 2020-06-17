e2f1265d528aac34f1762a893044ee7c8569e8ed
Mike F <sake.god@gmail.com>
2020-06-17 10:48:34 -0600

    Comments added to multi-dc-ag-failover

--- MSSQL/multi-dc-ag-failover.ps1
+++ MSSQL/multi-dc-ag-failover.ps1
@@ -17,32 +17,43 @@
 
 #>
 
-param([Parameter(ParameterSetName='Core')]
+param(#Availability Group Name
+      [Parameter(ParameterSetName='Core')]
       [string]$agname,
+      #SLA Domain to be assigned
       [Parameter(ParameterSetName='Enable')]
       [Alias("Enable","SLA")]
       [String]$SLAName,
+      #Log backup frequecny in minutes
       [Parameter(ParameterSetName='Enable')]
       [Alias("LogBackupFrequency")]
       [int]$LogBackupFrequencyMin,
+      #Log backup retention in days
       [Parameter(ParameterSetName='Enable')]
       [Alias("LogBackupRetention")]
       [int]$LogBackupRetentionDay,
+      #Trigger new snapshot when enabling protection
       [Parameter(ParameterSetName='Enable')]
       [Switch]$NewSnapshot,
+      #disable protection
       [Parameter(ParameterSetName='Disable')]
       [Switch]$Disable
       )
 
+#gather databases to be altered based on AG name
 $dbs = Get-RubrikDatabase -AvailabilityGroupName $agname
 
+#If an SLA is declared, protection is being enabled. 
 if($SLAName.Length -gt 0) {
       $dbs | Set-RubrikDatabase -SLA $SLAName -LogBackupFrequencyInSeconds ($LogBackupFrequencyMin * 60) -LogRetentionHours ($LogBackupRetentionDay * 24) -Confirm:$false
 
+      #if NewSnapshot is flagged, execute a new on-demand snapshot once protection is re-enabled
       if($NewSnapshot){
             $dbs | New-RubrikSnapshot -SLA $SLAName -Confirm:$false
       }
 }
+
+#if the Disable switch is flagged, 
 if($Disable){
       $dbs | Set-RubrikDatabase -SLA UNPROTECTED -LogBackupFrequencyInSeconds 0 -LogRetentionHours 0 -Confirm:$false
 }
