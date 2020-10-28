037f29d5ef2266846f5cc051eb1d9ed4ffd6f0f2
Damani <damani@rubrik.com>
2020-10-28 15:39:37 -0700

    Updated Help Text and comments

--- RVC/Invoke-RubrikVirtualCluster.ps1
+++ RVC/Invoke-RubrikVirtualCluster.ps1
@@ -1,32 +1,87 @@
 <#
     .SYNOPSIS
-        Will deploy a 4 node Rubrik Virtual Cluster on VMware
+        Deploys a Rubrik Virtual Cluster on VMware and adds data disks
 
     .DESCRIPTION
-        Will deploy a Rubrik Virtual Cluster on VMware.
+        This script deploy a Rubrik Virtual Cluster on VMware. It will also add the data disks to the cluster in VMware.
         This script requires that PowerCLI be installed and is in the users path.
+        This script requires that OVFTool be installed in the users path. 
 
-    .PARAMETER JobFile
-        None
+    .PARAMETER RVCDataDiskSize
+        Size of RVC data disks to add. 
+
+    .PARAMETER RVCDataDiskType
+        RVC Data Disk Type to Add. The officially supported type is EagerZeroedThick. Thick and Thin can only be used for testing.
+
+    .PARAMETER RVCDataNetwork
+        The Data Network to attach RVC to. This is the name of the network or switch port in VMware.
+
+    .PARAMETER RVCManagementNetwork
+        The Management Network to attach RVC to. This is the name of the network or switch port in VMware.
+
+    .PARAMETER RVCName
+        Name of the Rubrik Virtual Cluster. Nodes will be named by adding a dash and increasing number will be appended during deployment.
+
+    .PARAMETER RVCNumNodes
+        Number of RVC Nodes to deploy.
+
+    .PARAMETER RVCNumDataDisks
+        Number of data disks to add to each node.
+
+    .PARAMETER RVCOSDataDiskType
+        The disk mode with which RVC will be deployed. The officially supported type is EagerZeroedThick. Thick and Thin can only be used for testing.
+    
+    .PARAMETER RVCOVAFile
+        The OVA file to deploy RVC from.
+
+    .PARAMETER VMwareCluster
+        The VMware cluster in which to deploy RVC.
+
+    .PARAMETER VMwareCredentialFile
+        Credentials file for vCenter.
+        Use the following commands to create a credentials file:
+
+            $cred = Get-Credential
+            $cred | Export-Clixml C:\temp\VMwareCred.xml -Force
+    
+    .PARAMETER VMwareDataCenter
+        The VMware data center in which to deploy RVC.
+
+    .PARAMETER VMwareDataStore
+        The VMware datastore on which RVC will be deployed.
+
+    .PARAMETER VMwareVCenter
+        Hostname of the vCenter server.
+
+    .PARAMETER VMwareVMFolder
+        The VMware VM folder in which RVC will be deployed.
+    
+    .PARAMETER ConfigFile
+        Configuration file with parameters.
+
+    .PARAMETER RemoveCPUReservation
+        (Experimental) Remove CPU Reservation.
 
     .INPUTS
-        Required to updated the following parameters:
-            $Username - This will be a vCD Admin User
-            $Password - This will be a vCD Admin User Password
-            $vCDHost - The address of the vCD Cell you want to export the metadata for
-
+        None
     .OUTPUTS
         None
     .EXAMPLE
-        Creating credentials file: 
-            $cred = Get-Credential
-            $cred | Export-Clixml C:\temp\VMwareCred.xml -Force
+        $cred = Get-Credential
+        PS > $cred | Export-Clixml C:\temp\VMwareCred.xml -Force
 
-        Deploying RVC:
-        ./Invoke-RubrikVirtualCluster.ps1 options
+    .EXAMPLE
+        ./Invoke-RubrikVirtualCluster.ps1 -ConfigFile ./Sample-Invoke-RubrikVirtualClusterConfig.ps1
+    
+    .EXAMPLE
+        ./Invoke-RubrikVirtualCluster.ps1 -RVCDataDiskSize 6144 -RVCDataDiskType "EagerZeroedThick" -RVCDataNetwork "myFastDataNetwork" -RVCManagementNetwork "myAwesomeManagementNetwork" -RVCName "myCoolClusterName" -RVCNumNodes 4 -RVCNumDataDisks 6 -RVCOSDiskType "EagerZeroedThick" -RVCOVAFile "rubrik-vc-vr6412-esx-5.3.0-123456.ova" -VMwareCluster "myNiceVMwareCluster" -VMwareCredentialFile "myFantasticVCenterCreds.xml" -VMwareDataCenter "myLovelyVMwareDataCenter" -VMwareDataStore "myBigDataStore" -VMwareVCenter "myFantasticVCenter" -VMwareVMFolder "myStupendousVMFolderDir/myGreatVMFolderName"
 
     .LINK
-        None
+        https://github.com/rubrikinc/rubrik-scripts-for-powershell
+    .LINK
+        https://www.vmware.com/support/developer/PowerCLI/
+    .LINK
+        https://code.vmware.com/web/tool/4.4.0/ovf
 
     .NOTES
         Name:       Deploy Rubrik Virtual Cluster (RVC)
@@ -105,7 +160,7 @@ param
         ParameterSetName = "Config File")]
     [string] $ConfigFile,
 
-    [Parameter(Mandatory = $false, HelpMessage = "Remove CPU Reservation.")]
+    [Parameter(Mandatory = $false, HelpMessage = "(Experimental) Remove CPU Reservation.")]
     [switch] $RemoveCPUReservation = $false
 
 )
@@ -113,6 +168,19 @@ param
 if ($ConfigFile) {
     . $ConfigFile
 }
+
+if ($null -eq (Get-Command "ovftool.exe" -ErrorAction SilentlyContinue) -And $null -eq (Get-Command "ovftool" -ErrorAction SilentlyContinue)) { 
+    Write-Host "Unable to find ovftool in your PATH."
+    Write-Host "Verify that ovftool is installed."
+    exit 1
+}
+
+if ((Get-Command "Connect-VIServer" -ErrorAction SilentlyContinue)) { 
+    Write-Host "Unable to find Connect-VIServer in your PATH"
+    Write-Host "Make sure that PowerCLI is installed."
+    exit 1
+}
+ 
 
 Import-Module VMware.VimAutomation.Core
 
@@ -125,6 +193,7 @@ Connect-VIServer $VMwareVCenter -Credential $VMwareCreds
 # $myDatastore = Get-Datastore -Name $DataStore
 # $myVMFolder = Get-Folder -Name $VMFolder
 
+$node_config = @{}
 for ($myRVCNum = 1; $myRVCNum -le $RVCNumNodes; $myRVCNum++) {
 
     $myRVCName = "$RVCName-$myRVCNum"
@@ -159,7 +228,10 @@ for ($myRVCNum = 1; $myRVCNum -le $RVCNumNodes; $myRVCNum++) {
             $myVM | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CpuReservationMhz 0
         }
     }
-    $myVM | Start-VM
+    # # Gather Bootstrap Info for node.
+    #     $myVM | Start-VM
+    #     $myNodeName = $myVM | select @{N = 'FQDN'; E = { $_.ExtensionData.Guest.IPStack[0].DnsConfig.HostName}
+    #     }
 }
 
 # Bootstrap cluster
