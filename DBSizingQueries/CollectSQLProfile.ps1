7ad4a64db2687ac8870d8e84f73075f09ab9a1ea
Jason Froebe <jason.froebe@gmail.com>
2020-12-01 11:53:55 -0500

    Add ability in CollectSQLProfile.ps1 to read instances from a file Fixes #163

--- DBSizingQueries/CollectSQLProfile.ps1
+++ DBSizingQueries/CollectSQLProfile.ps1
@@ -6,6 +6,7 @@ param(
     ,[Switch] $Anonymize
     ,[string] $SqlUser
     ,[string] $SqlPassword
+    ,[String] $InstanceCSVFile
 )
 BEGIN{
     if(Get-Module -ListAvailable SqlServer){Import-Module SqlServer}
@@ -14,6 +15,32 @@ BEGIN{
     $queries = Get-ChildItem $QueryPath -Filter "*.sql"
     $queries | ForEach-Object {$_ | Add-Member -MemberType NoteProperty -Name FileName -Value "$($_.Name.Replace('.sql',''))-$(Get-Date -Format 'yyyyMMddHHmm').csv"}
     $header = $true
+
+    # If specified, the host/instance entries in the csv file will be appended to the $SQLInstance array of String objects
+    #
+    # Format of CSV file:
+    #
+    # host,instance
+    # myhost,ZeSQLInstance
+    if (Test-Path $InstanceCSVFile -PathType leaf)
+    {
+        $instObjArray = Import-CSV -path $InstanceCSVFile
+
+        $instObjArray | ForEach-Object {
+            [String] $tmpInstance
+
+            if($_.instance -eq 'default')
+            {
+                $tmpInstance = $_.host
+            }
+            else
+            {
+                $tmpInstance = ("[0]\[1]" -f $_.host, $_.instance)
+            }
+
+            $SQLInstance += $tmpInstance
+        }
+    }
 }
 
 PROCESS
@@ -59,6 +86,7 @@ PROCESS
             $output = ""
         }
         $header = $false
-        }
     }
+}
+
 END{}
